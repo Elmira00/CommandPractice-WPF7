2d5ca3b8d2ba69a93f847c68663b457fff8e6c3f
Ahmed_pz48 <Ahmed_pz48@itstep.edu.az>
2024-02-14 22:44:30 +0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp54
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set { user = value;OnPropertyChanged(); }
+        }
+        public SubmitCommand SubmitCommand { get; set; }
+        public MainWindow()
+        {
+            User = new User();
+            InitializeComponent();
+            this.DataContext = this;
+            SubmitCommand = new SubmitCommand(() =>
+            {
+                var serializer = new JsonSerializer();
+                using (var sw = new StreamWriter("user.json"))
+                {
+                    using (var jw = new JsonTextWriter(sw))
+                    {
+                        jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                        serializer.Serialize(jw, User);
+                    }
+                }
+                MessageBox.Show("Submitted successfully !");
+            });
+        }
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp54
+{
+    public class User:INotifyPropertyChanged
+    {
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set { name = value; OnPropertyChanged(); }
+		}
+		private string surname;
+
+		public string Surname
+		{
+			get { return surname; }
+			set { surname = value; OnPropertyChanged(); }
+		}
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; OnPropertyChanged(); }
+		}
+		private int score;
+
+		public int Score
+		{
+			get { return score; }
+			set { score = value; OnPropertyChanged(); }
+		}
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+
+
+    }
+}
